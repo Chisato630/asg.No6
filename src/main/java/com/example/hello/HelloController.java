4b4104605605a6ae291be48ecd783de00e8c4e9e
Chisato <153170893+Chisato630@users.noreply.github.com>
2024-02-22 17:43:11 +0900

    hello world をレスポンスする処理の作成

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+    public HelloResponse hello(){
+        return new HelloResponse("hello world");
     }
 }
