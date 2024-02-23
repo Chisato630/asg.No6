29c4151b484486ca485169e8345426842e8b59da
Chisato <153170893+Chisato630@users.noreply.github.com>
2024-02-23 16:17:57 +0900

    PUTメソッドによる goodbye! をレスポンスする処理を追加

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public HelloResponse hello(){
         return new HelloResponse("hello!");
+    }
+
+    @PutMapping("/goodbye")
+    public HelloResponse goodbye(){
+        return new HelloResponse("goodbye!");
     }
 }
