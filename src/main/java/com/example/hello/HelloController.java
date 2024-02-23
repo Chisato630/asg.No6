6a76f2e014cdf304b63b07e812645af05fc03b57
Chisato <153170893+Chisato630@users.noreply.github.com>
2024-02-23 17:12:02 +0900

    Revert "第6回課題： hello world をレスポンスする処理の作成"

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,20 +1,13 @@
 package com.example.hello;
 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public HelloResponse hello(){
-        return new HelloResponse("hello!");
-    }
-
-    @PutMapping("/goodbye")
-    public HelloResponse goodbye(){
-        return new HelloResponse("goodbye!");
+    public String hello(){
+        return "hello world";
     }
 }
